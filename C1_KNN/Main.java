fada4c2b9d582419218e82d4efc5718eb5b84f54
Sebastian <94530856+Schmirito@users.noreply.github.com>
2023-12-14 08:35:22 +0000

    knn

--- C1_KNN/Main.java
+++ C1_KNN/Main.java
@@ -1,45 +1,67 @@
 package C1_KNN;
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Main {
+
+    static int a,b;
+    static int k = 3;
+    static double distanz;
+    static Optional<Double> min;
+    static String antw;
+
+    static double[] mins = new double[k];
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         ArrayList<ObjektKlasse> obj = new ArrayList<>();
-        obj.add(new ObjektKlasse(3,4,"Katze"));
-        obj.add(new ObjektKlasse(3,8,"Katze"));
-        obj.add(new ObjektKlasse(4,4,"Katze"));
-        obj.add(new ObjektKlasse(4,6,"Katze"));
-        obj.add(new ObjektKlasse(5,3,"Katze"));
-        obj.add(new ObjektKlasse(5,5,"Katze"));
-        obj.add(new ObjektKlasse(5,7,"Katze"));
-        obj.add(new ObjektKlasse(6,7,"Katze"));
-        obj.add(new ObjektKlasse(6,9,"Katze"));
-        obj.add(new ObjektKlasse(7,8,"Katze"));
-        obj.add(new ObjektKlasse(7,9,"Katze"));
-        obj.add(new ObjektKlasse(8,9,"Katze"));
-        obj.add(new ObjektKlasse(8,10,"Katze"));
-        obj.add(new ObjektKlasse(9,9,"Katze"));
-        obj.add(new ObjektKlasse(10,8,"Katze"));
-        obj.add(new ObjektKlasse(11,9,"Katze"));
-        obj.add(new ObjektKlasse(8,2,"Hund"));
-        obj.add(new ObjektKlasse(8,3,"Hund"));
-        obj.add(new ObjektKlasse(8,5,"Hund"));
-        obj.add(new ObjektKlasse(8,7,"Hund"));
-        obj.add(new ObjektKlasse(9,1,"Hund"));
-        obj.add(new ObjektKlasse(9,4,"Hund"));
-        obj.add(new ObjektKlasse(10,2,"Hund"));
-        obj.add(new ObjektKlasse(10,3,"Hund"));
-        obj.add(new ObjektKlasse(10,5,"Hund"));
-        obj.add(new ObjektKlasse(10,6,"Hund"));
-        obj.add(new ObjektKlasse(10,9,"Hund"));
-        obj.add(new ObjektKlasse(11,7,"Hund"));
-        obj.add(new ObjektKlasse(12,2,"Hund"));
-        obj.add(new ObjektKlasse(12,4,"Hund"));
-        obj.add(new ObjektKlasse(13,7,"Hund"));
+        ArrayList<Double> dst = new ArrayList<>();
+
+        obj.add(new ObjektKlasse(72,20,0));
+        obj.add(new ObjektKlasse(89,12,0));
+        obj.add(new ObjektKlasse(94,6,0));
+        obj.add(new ObjektKlasse(96,3,0));
+        obj.add(new ObjektKlasse(97,2,0));
+
+        obj.add(new ObjektKlasse(77,7,1));
+        obj.add(new ObjektKlasse(84,10,1));
+        obj.add(new ObjektKlasse(85,14,1));
+        obj.add(new ObjektKlasse(87,9,1));
+        obj.add(new ObjektKlasse(91,2,1));
+        obj.add(new ObjektKlasse(92,10,1));
+        obj.add(new ObjektKlasse(93,17,1));
+
+        obj.add(new ObjektKlasse(75,25,2));
+        obj.add(new ObjektKlasse(77,11,2));
+        obj.add(new ObjektKlasse(80,17,2));
+        obj.add(new ObjektKlasse(87,15,2));
+
+        System.out.println("Neuen Wert hinzufügen?");
+        antw = sc. next();
+        if(antw == "Ja" || antw == "ja"){
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        for (ObjektKlasse objektKlasse : obj) {
+            distanz = berechneDistanz(a,b,objektKlasse.x, objektKlasse.y);
+            dst.add(distanz);          
+        }
+        for (int i = 0; i < k; i++) {
+            mins[k] = getMin(dst);
+        }
+        
+
+        }
     }
     
 
-}
+public static double berechneDistanz(int a, int b, int x, int y){
+    return Math.abs((a - x) + (b - y));
 
-public static double distanz(ArrayList obj){
-        
+}
+public static Optional getMin(ArrayList dst){
+    return dst.stream().min(null);
+}
 }
