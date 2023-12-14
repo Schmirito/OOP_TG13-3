7a5008416cf41e89523aa31f0404fa7139ba7663
Sebastian <94530856+Schmirito@users.noreply.github.com>
2023-12-14 08:35:13 +0000

    knn

--- C1_KNN/ObjektKlasse.java
+++ C1_KNN/ObjektKlasse.java
@@ -1,10 +1,10 @@
 package C1_KNN;
 public class ObjektKlasse {
-    int x;
-    int y;
-    String name;
+    public int x;
+    public int y;
+    public int name;
 
-    public ObjektKlasse(int x, int y, String name){
+    public ObjektKlasse(int x, int y, int name){
         this.x = x;
         this.y = y;
         this.name = name;
